3d0829b864b5e3bcabdf44eeb9eca0f4af662e23
NikitaT <654070@gmail.com>
2014-09-30 13:06:45 +0600

    initial commit

--- constraintsTest/ViewController.m
+++ constraintsTest/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UILabel *label;
 
 @end
 
@@ -16,7 +17,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self addTextWithCompletionBlock:^{
+        [self addTextWithCompletionBlock:^{
+            [self addTextWithCompletionBlock:nil];
+        }];
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +29,14 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void) addTextWithCompletionBlock:(void (^)()) completionBlock {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.label.text = [NSString stringWithFormat:@"Some text: %@", self.label.text];
+            if (completionBlock) {
+                completionBlock();
+            }
+        });
+    });
+}
 @end
