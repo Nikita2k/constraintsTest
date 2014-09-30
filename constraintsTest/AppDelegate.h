1635e06c796bb6b3c6e0aa3bf4ade90196fa649d
NikitaT <654070@gmail.com>
2014-09-30 12:50:49 +0600

    Initial Commit

---
+++ constraintsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  constraintsTest
+//
+//  Created by Никита Тук on 30.09.14.
+//  Copyright (c) 2014 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ constraintsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  constraintsTest
+//
+//  Created by Никита Тук on 30.09.14.
+//  Copyright (c) 2014 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
